1332b72f2cd8d323d8658fb3eb15a9b413c6e590
tqb310 <baotq310@gmail.com>
2021-10-22 12:24:12 +0700

    Initilize prj

---
+++ src/features/Test 2 parent/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Demo2 from '../Test 2'
+
+export default function Demo() {
+    return (
+        <div>
+            {/* <Demo2/> */}
+        </div>
+    )
+}
---
+++ src/features/Test 2/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+export default function Demo2() {
+    return (
+        <div>
+            <p className="text">Test thoi nha - text 2 child</p>
+        </div>
+    )
+}

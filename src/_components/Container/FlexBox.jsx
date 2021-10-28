5a475991d3443a52eb8c3b919b1820c6618e1401
tqb310 <baotq310@gmail.com>
2021-10-28 20:35:07 +0700

    Done Date&Time in header

---
+++ src/_components/Container/FlexBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Box} from '@mui/material';
+import {styled} from '@mui/material/styles';
+
+export const FlexBox = styled(Box)`
+    display: flex;
+    flexDirection: row;
+`
+
+export const InFlexBox = styled(Box)`
+    display: inline-flex;
+    
+`
+
--- src/_components/header/_components/DateTime/index.jsx
+++ src/_components/header/_components/DateTime/index.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import PropTypes from 'prop-types'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Box} from '@mui/material';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Box } from "@mui/material";
 import "./index.scss";
 
 function DateTime(props) {
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
+  
+  useEffect(() => {
+    getDateTime(setTime, setDate);
+  }, []);
+
   return (
-    <div>
-      <Box>
-        <FontAwesomeIcon icon="calendar-week" />
-        <span>10/28/2021</span>
+    <Box className="datetime">
+      <Box className="date">
+        <FontAwesomeIcon className="date__icon" icon="calendar-week" />
+        <span className="date__data">{date}</span>
       </Box>
       <Box>
-        <FontAwesomeIcon icon="clock" />
-        <span>11:26:12 AM</span>
+        <FontAwesomeIcon className="time__icon" icon="clock" />
+        <span className="time__data">{time}</span>
       </Box>
-    </div>
+    </Box>
   );
+}
+
+const getDateTime = (setTime, setDate) => {
+    setInterval(() => {
+        let date = new Date();
+        setTime(date.toLocaleTimeString());
+        setDate(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`);
+      }, 1000);
 }
 
 DateTime.propTypes = {};

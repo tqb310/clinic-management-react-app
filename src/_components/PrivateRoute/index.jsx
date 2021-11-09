c0780b6dc5b2a043ea08fda29d1b73432bc7d311
tqb310 <baotq310@gmail.com>
2021-11-09 18:58:42 +0700

    Edit sidebar

--- src/_components/PrivateRoute/index.jsx
+++ src/_components/PrivateRoute/index.jsx
@@ -4,20 +4,20 @@ import authentication from '_services/authentication.service';
 import PropTypes from 'prop-types';
 
 //Fake user data storage
-const currentUser = {name: 'Bao', role: '1', jwt: '1234'};
+//const currentUser = {name: 'Bao', role: '1', jwt: '1234'};
 const Unauthorized = () => (<div>Tài khoản của bạn không có quyền truy cập trang này</div>)
 
 function PrivateRoute({component: Component, roles, ...rest}) {
     return (
        <Route {...rest} render={
            (props) => {
-               if(!authentication.getCurrentUser()){
-                   return <Redirect to={{pathname: '/dang-nhap', state: {from: props.location}}}/>
-               }
+            //    if(!authentication.getCurrentUser()){
+            //        return <Redirect to={{pathname: '/dang-nhap', state: {from: props.location}}}/>
+            //    }
 
-               if(roles && roles.localeCompare(authentication.getCurrentUser().payload.role)){
-                   return <Unauthorized {...props}/>
-               }
+            //    if(roles && roles.localeCompare(authentication.getCurrentUser().payload.role)){
+            //        return <Unauthorized {...props}/>
+            //    }
 
                return <Component {...props}/>
            }

ac09520118bce8dac42b96eaa9e53d25f79f3ab6
tqb310 <baotq310@gmail.com>
2021-11-10 07:46:49 +0700

    Set up TabTableWrapper component

--- src/ReceptionPages/4-query/index.jsx
+++ src/ReceptionPages/4-query/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import TabTableWrapper from "_components/TabTableWrapper";
+
+const data = [
+    {title: 'Tất cả', number: 50},
+    {title: 'Đang khám', number: 2},
+    {title: 'Đang chờ', number: 10},
+    {title: 'Qua lượt', number: 4},
+]
 
 function Query(props) {
     return (
         <div>
-            Hàng đợi
+            <TabTableWrapper tabNameArr={data}></TabTableWrapper>
         </div>
     )
 }
---
+++ src/_components/TabTableWrapper/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import {Box} from '@mui/material';
+import './index.scss'
+
+
+
+function TabTableWrapper({tabNameArr, children, ...rest}) {
+    return (
+        <Box className="TabTableWrapper">
+            Hang doi
+        </Box>
+    )
+}
+
+TabTableWrapper.propTypes = {
+
+}
+
+export default TabTableWrapper
+

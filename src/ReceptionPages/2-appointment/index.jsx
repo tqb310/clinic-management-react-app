d8ea6a2b28a324e501f7271972f5e629d903d576
liemdang260 <69330905+liemdang260@users.noreply.github.com>
2021-11-16 14:58:24 +0700

    commit

--- src/ReceptionPages/2-appointment/index.jsx
+++ src/ReceptionPages/2-appointment/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Infomation from './infomation';
 import Content from './content';
 import AppoimentDemand from './rightbar';
+import socket from '_services/socket.io'
 import './index.scss'
 // import PropTypes from 'prop-types';
 
 function Appointment(props) {
+    socket.on('AppointmentListChange',(appointment)=>{
+        console.log(appointment)
+    })
     return (
         <div className='appointment-container'>
             <Infomation></Infomation>

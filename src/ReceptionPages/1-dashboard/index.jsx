07afca174b23735a7ac3f6a775569fe524722ae5
tqb310 <baotq310@gmail.com>
2021-11-17 11:34:59 +0700

    Done UI dashboard

--- src/ReceptionPages/1-dashboard/index.jsx
+++ src/ReceptionPages/1-dashboard/index.jsx
@@ -1,13 +1,36 @@
 import React from "react";
-import './index.scss';
+import CardContainer from "./_components/CardContainer";
+import VisitChart from "./_components/VisitChart";
+import RoomState from "./_components/RoomState";
+import Appointment from "./_components/Appointment";
+import {RightBar} from "_components/StyledComponent"
+import "./index.scss";
 // import { Scrollbars } from "react-custom-scrollbars-2";
 // import {RightBar} from '_components/StyledComponent/RightBar';
 // import PropTypes from 'prop-types'
 
-function Dashboard(props) {  
+function Dashboard(props) {
   return (
     <div className="dashboard">
-      <p className="dashboard__title">Xin chào, <span>Nguyễn Văn A</span></p>
+      <p className="dashboard__title">
+        Xin chào, <span>Nguyễn Văn A</span>
+      </p>
+      <div className="dashboard__cards">
+        <CardContainer />
+      </div>
+      <div className="dashboard__averageVisitChart">
+          <VisitChart/>
+      </div>
+      <div className="dashboard__roomState">
+          <RoomState/>
+      </div>
+      <div className="dashboard__appointment">
+          <Appointment/>
+      </div>
+      <RightBar>
+        <div>Hoạt động gần đây</div>
+        {/* <div>Bệnh nhân</div> */}
+      </RightBar>
     </div>
   );
 }

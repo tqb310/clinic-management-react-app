fa9039328cacdeffd1e45175c3beb74300277ee4
tqb310 <baotq310@gmail.com>
2022-03-31 23:06:48 +0700

    update queue ui in checkup page

--- src/pages/2-examine/_components/ExamineCard/index.jsx
+++ src/pages/2-examine/_components/ExamineCard/index.jsx
@@ -1,141 +1,168 @@
-import React, { useState } from "react";
-import { Scrollbars } from "react-custom-scrollbars-2";
-import { TextField, Button } from "@mui/material";
-import { RadioButtonUnchecked, CheckCircle } from "@mui/icons-material";
-import { CustomPaper } from "_components/shared/StyledComponent";
-import PatientInfo from "../PatientInfo";
-import ServiceInfo from "../ServiceInfo";
-import ExaminingInfo from "../ExaminingInfo";
-import PrescriptionForm from "./Prescription";
-import "./index.scss";
+import React, {useState} from 'react';
+import {TextField, Button} from '@mui/material';
+import {
+    RadioButtonUnchecked,
+    CheckCircle,
+} from '@mui/icons-material';
+import {CustomPaper} from '_components/shared/StyledComponent';
+import PatientInfo from '../PatientInfo';
+import ServiceInfo from '../ServiceInfo';
+import ExaminingInfo from '../ExaminingInfo';
+import PrescriptionForm from './Prescription';
+import './index.scss';
 // import PropTypes from 'prop-types'
 
-function ExamineCard({ data, continuous, handleSubmitFinal }) {
-  console.log(data)
-  const [open, setOpen] = useState(false);
-  // const [isValidForm, setValidForm] = useState(true);
-  const [prescriptionData, setPrescriptionData] = useState([]);
-  const [examiningData, setExaminingData] = useState({});
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleSubmitPres = (data) => {
+function ExamineCard({
+    data,
+    continuous,
+    handleSubmitFinal,
+}) {
     console.log(data);
-    setPrescriptionData(data);
-  };
-  const handleSubmitExamining = (data) => {
-    //  console.log("FINAL: ", data);
-    setExaminingData(data);
-  };
-  const handleSubmitComplete = () => {
-    if (prescriptionData.length === 0) {
-      alert("Bạn chưa kê toa thuốc");
-      return;
-    }
-    if (Object.keys(examiningData).length === 0) {
-      alert("Bạn chưa điền kết quả khám lâm sàng");
-      return;
-    }
-    // console.log(examiningData);
-    const result = {
-      ...examiningData,
-      PRESCRIPTION: prescriptionData,
+    const [open, setOpen] = useState(false);
+    // const [isValidForm, setValidForm] = useState(true);
+    const [prescriptionData, setPrescriptionData] =
+        useState([]);
+    const [examiningData, setExaminingData] = useState({});
+    const handleClose = () => {
+        setOpen(false);
     };
-    handleSubmitFinal(result);
-  };
-  return (
-    <Scrollbars style={{ width: "100%", height: "100%" }} autoHide={true}>
-      <CustomPaper className="DTExamineCard">
-        <div className="DTExamineCard__header">
-          <p>Phiếu khám bệnh</p>
-          <p>
-            <span>{data.diagnostic.DIAGNOSTIC_ID}</span>
-          </p>
-        </div>
-        <div className="DTExamineCard__cardInfo">
-          <p>
-            <span>Ngày lập</span>
-            <TextField
-              variant="filled"
-              value="13/12/2021"
-              size="small"
-              sx={{
-                "& .MuiInputBase-input": {
-                  paddingTop: 0,
-                },
-                width: 150,
-              }}
+    const handleSubmitPres = data => {
+        console.log(data);
+        setPrescriptionData(data);
+    };
+    const handleSubmitExamining = data => {
+        //  console.log("FINAL: ", data);
+        setExaminingData(data);
+    };
+    const handleSubmitComplete = () => {
+        if (prescriptionData.length === 0) {
+            alert('Bạn chưa kê toa thuốc');
+            return;
+        }
+        if (Object.keys(examiningData).length === 0) {
+            alert('Bạn chưa điền kết quả khám lâm sàng');
+            return;
+        }
+        // console.log(examiningData);
+        const result = {
+            ...examiningData,
+            PRESCRIPTION: prescriptionData,
+        };
+        handleSubmitFinal(result);
+    };
+    return (
+        <CustomPaper className="DTExamineCard">
+            <div className="DTExamineCard__header">
+                <p>Phiếu khám bệnh</p>
+                <p>
+                    <span>
+                        {data.diagnostic.DIAGNOSTIC_ID}
+                    </span>
+                </p>
+            </div>
+            <div className="DTExamineCard__cardInfo">
+                <p>
+                    <span>Ngày lập</span>
+                    <TextField
+                        variant="filled"
+                        value="13/12/2021"
+                        size="small"
+                        sx={{
+                            '& .MuiInputBase-input': {
+                                paddingTop: 0,
+                            },
+                            width: 150,
+                        }}
+                    />
+                </p>
+                <p>
+                    <span>Nhân viên tiếp nhận</span>
+                    <TextField
+                        variant="filled"
+                        value="Đặng Ngọc Liêm"
+                        size="small"
+                        sx={{
+                            '& .MuiInputBase-input': {
+                                paddingTop: 0,
+                            },
+                            width: 200,
+                        }}
+                    />
+                </p>
+            </div>
+            <div className="DTExamineCard__content">
+                <div className="DTExamineCard__patientInfo">
+                    <PatientInfo
+                        data={data.diagnostic.PATIENT}
+                    />
+                </div>
+                <div className="DTExamineCard__service">
+                    <ServiceInfo
+                        data={
+                            data.diagnostic
+                                .SERVICE_ID_SERVICEs
+                        }
+                    />
+                </div>
+                <div className="DTExamineCard__examineInfo">
+                    <ExaminingInfo
+                        handleSubmit={handleSubmitExamining}
+                    />
+                </div>
+            </div>
+            <div className="DTExamineCard__footer">
+                <div>
+                    <Button
+                        variant="outlined"
+                        sx={{width: 180, marginRight: 2}}
+                    >
+                        Cập nhật dịch vụ
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        color={
+                            prescriptionData.length > 0
+                                ? 'success'
+                                : 'warning'
+                        }
+                        startIcon={
+                            prescriptionData.length > 0 ? (
+                                <CheckCircle />
+                            ) : (
+                                <RadioButtonUnchecked />
+                            )
+                        }
+                        sx={{width: 110}}
+                        onClick={() => setOpen(true)}
+                    >
+                        Kê toa
+                    </Button>
+                </div>
+                <div>
+                    <Button
+                        variant="outlined"
+                        sx={{width: 110, marginRight: 2}}
+                        // disabled={isValidForm}
+                        onClick={handleSubmitComplete}
+                    >
+                        Hoàn tất
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="error"
+                        sx={{width: 110}}
+                    >
+                        Hủy
+                    </Button>
+                </div>
+            </div>
+            <PrescriptionForm
+                open={open}
+                handleClose={handleClose}
+                handleSubmit={handleSubmitPres}
             />
-          </p>
-          <p>
-            <span>Nhân viên tiếp nhận</span>
-            <TextField
-              variant="filled"
-              value="Đặng Ngọc Liêm"
-              size="small"
-              sx={{
-                "& .MuiInputBase-input": {
-                  paddingTop: 0,
-                },
-                width: 200,
-              }}
-            />
-          </p>
-        </div>
-        <div className="DTExamineCard__content">
-          <div className="DTExamineCard__patientInfo">
-            <PatientInfo data={data.diagnostic.PATIENT}/>
-          </div>
-          <div className="DTExamineCard__service">
-            <ServiceInfo data={data.diagnostic.SERVICE_ID_SERVICEs}/>
-          </div>
-          <div className="DTExamineCard__examineInfo">
-            <ExaminingInfo handleSubmit={handleSubmitExamining} />
-          </div>
-        </div>
-        <div className="DTExamineCard__footer">
-          <div>
-            <Button variant="outlined" sx={{ width: 180, marginRight: 2 }}>
-              Cập nhật dịch vụ
-            </Button>
-            <Button
-              variant="outlined"
-              color={prescriptionData.length > 0 ? "success" : "warning"}
-              startIcon={
-                prescriptionData.length > 0 ? (
-                  <CheckCircle />
-                ) : (
-                  <RadioButtonUnchecked />
-                )
-              }
-              sx={{ width: 110 }}
-              onClick={() => setOpen(true)}
-            >
-              Kê toa
-            </Button>
-          </div>
-          <div>
-            <Button
-              variant="outlined"
-              sx={{ width: 110, marginRight: 2 }}
-              // disabled={isValidForm}
-              onClick={handleSubmitComplete}
-            >
-              Hoàn tất
-            </Button>
-            <Button variant="contained" color="error" sx={{ width: 110 }}>
-              Hủy
-            </Button>
-          </div>
-        </div>
-        <PrescriptionForm
-          open={open}
-          handleClose={handleClose}
-          handleSubmit={handleSubmitPres}
-        />
-      </CustomPaper>
-    </Scrollbars>
-  );
+        </CustomPaper>
+    );
 }
 
 // ExamineCard.propTypes = {};
--- src/pages/2-examine/_components/RightBar/index.jsx
+++ src/pages/2-examine/_components/RightBar/index.jsx
@@ -1,117 +1,169 @@
-import React, { useState } from "react";
-import { RightBar } from "_components/shared/StyledComponent";
-import { Button } from "@mui/material";
-import { SwapVert, ArrowBackIosNew, Alarm } from "@mui/icons-material";
-import socket from '_services/socket.io'
-import "./index.scss";
+import React, {useState, memo} from 'react';
+import {RightBar} from '_components/shared/StyledComponent';
+import {Button, Typography, Box} from '@mui/material';
+import {SwapVert, Alarm} from '@mui/icons-material';
+import {Dot} from '_components/shared/StyledComponent';
+// import socket from '_services/socket.io';
+import './index.scss';
 // import PropTypes from 'prop-types'
 
-const QueueItem = ({ avatar, pname, pnum, cid, state, current, start }) => {
-  return (
-    <div className="RBDoctorHome__queueItem">
-      <div style={{ display: "flex", alignItems: "center" }}>
-        {current && <ArrowBackIosNew></ArrowBackIosNew>}
-        <div className="RBDoctorHome__avatar">
-          <div className="RBDoctorHome__avatarInner">{avatar}</div>
-        </div>
-        <div className="RBDoctorHome__patient">
-          <p>#{cid}</p>
-          <p>{pname}</p>
-        </div>
-        <div className="RBDoctorHome__orderNum">
-          <div className="RBDoctorHome__orderNumInner">{pnum}</div>
-        </div>
-      </div>
-      {current && (
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            padding: '0 1rem'
-          }}
+const QueueItem = ({
+    avatar,
+    pname,
+    pnum,
+    cid,
+    state,
+    current = false,
+    start,
+    className = '',
+}) => {
+    return (
+        <Box
+            className={`queue-list-doctor__item ${className}`}
         >
-          <div style={{ fontSize: '16px' }}>
-            <Alarm sx={{ fontSize: '16px', transform: 'translateY(1.8px)' }} /> 07:15
-          </div>
-          <Button
-            onClick={()=>start(pnum)}
-            variant="outlined"
-            color="success"
-            sx={{ fontSize: 12, marginTop: "5px" }}
-          >
-            Bắt đầu
-          </Button>
-        </div>
-      )}
-    </div>
-  );
+            <Typography
+                variant="h6"
+                sx={{
+                    width: '60px',
+                    color: 'primary.main',
+                    textAlign: 'center',
+                }}
+            >
+                # 26
+            </Typography>
+            <Box className="queue-list-doctor__item-info">
+                <Typography variant="h6">
+                    Truong Thi Lan
+                </Typography>
+                <Typography color="#888" fontWeight={400}>
+                    Nữ <Dot />{' '}
+                    <Typography
+                        component="span"
+                        fontWeight={400}
+                    >
+                        23/12/1999
+                    </Typography>
+                </Typography>
+                <Typography color="#888" fontWeight={400}>
+                    Xếp hàng vào lúc:{' '}
+                    <Typography
+                        component="span"
+                        fontWeight={400}
+                    >
+                        7:45 <Dot /> 30/03/2022
+                    </Typography>
+                </Typography>
+                {current && (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            mt: 1,
+                        }}
+                    >
+                        <Typography>
+                            <Alarm
+                                sx={{
+                                    fontSize: '1.8rem',
+                                    mr: 1,
+                                    verticalAlign: 'middle',
+                                }}
+                            />
+                            <Typography
+                                variant="h5"
+                                component="span"
+                                sx={{
+                                    verticalAlign: 'top',
+                                }}
+                            >
+                                15:00
+                            </Typography>
+                        </Typography>
+                        <Button
+                            sx={{
+                                textTransform: 'capitalize',
+                                py: 0.25,
+                            }}
+                            variant="outlined"
+                        >
+                            Bắt đầu
+                        </Button>
+                    </Box>
+                )}
+            </Box>
+        </Box>
+    );
 };
 
-function RightBarContent({ queue, continuous, handleContinuous, start }) {
-  const [selected, setSelected] = useState(1);
-  console.log(queue)
-  const createAvatar = (name) => {
-    return name.split(' ').map(data => data[0]).join("")
-  }
-  return (
-    <RightBar className="RBDoctorHome">
-      <h4>Hàng đợi</h4>
-      {(queue.length > 0) && <div>
-        {continuous && <QueueItem
-          avatar={createAvatar(queue[0].diagnostic.PATIENT.PATIENT_NAME)}
-          pname={queue[0].diagnostic.PATIENT.PATIENT_NAME}
-          cid={queue[0].diagnostic.DIAGNOSTIC_ID}
-          pnum={queue[0].order}
-          current={true}
-          start={start}
-        />}
-        <Button
-          onClick={()=>handleContinuous(true)}
-          variant="outlined"
-          startIcon={<SwapVert sx={{ color: "white" }} />}
-          sx={{
-            borderWidth: "2px",
-            marginTop: ".8rem",
-            borderColor: "#F5F8FE",
-            color: "#F5F8FE",
-            fontSize: "12px",
-            // backgroundColor: "#EFAD0A",
-            "&:hover": {
-              borderWidth: "2px",
-              borderColor: "#F5F8FE",
-              opacity: 0.8,
-            },
-          }}
-        >
-          Tiếp tục
-        </Button>
-        <div className="RBDoctorHome__queueTab">
-          <div className={selected === 1 ? "active" : ""}>
-            Đang chờ <span>20</span>
-          </div>
-          <div className={selected === 2 ? "active" : ""}>
-            Dịch vụ <span>20</span>
-          </div>
-          <div className={selected === 1 ? "active" : ""}>
-            Đang chờ <span>20</span>
-          </div>
-        </div>
-        <div className="RBDoctorHome__queueList">
-          {queue.slice((continuous ? 1 : 0)).map((item) => (
+function RightBarContent({
+    queue,
+    continuous,
+    handleContinuous,
+    start,
+}) {
+    const [selected, setSelected] = useState(1);
+    console.log(queue);
+    const createAvatar = name => {
+        return name
+            .split(' ')
+            .map(data => data[0])
+            .join('');
+    };
+    return (
+        <RightBar className="RBDoctorHome">
+            <Typography
+                variant="h5"
+                fontWeight={700}
+                gutterBottom
+            >
+                Hàng đợi
+            </Typography>
             <QueueItem
-              avatar={createAvatar(item.diagnostic.PATIENT.PATIENT_NAME)}
-              pname={item.diagnostic.PATIENT.PATIENT_NAME}
-              cid={item.diagnostic.DIAGNOSTIC_ID}
-              pnum={item.order}
+                avatar={createAvatar('Truong Bao')}
+                pname="Truong Quoc Bao"
+                cid={12345}
+                pnum={12345}
+                current
+                start={() => {}}
             />
-          ))}
-        </div>
-      </div>}
-    </RightBar>
-  );
+
+            <Button
+                onClick={() => handleContinuous(true)}
+                variant="outlined"
+                startIcon={<SwapVert />}
+                sx={{my: 2}}
+            >
+                Tiếp tục
+            </Button>
+            <div className="RBDoctorHome__queueTab">
+                <div
+                    className={`RBDoctorHome__queueTab-item
+                        ${selected === 1 ? 'active' : ''}
+                    `}
+                >
+                    Đang chờ <span>(20)</span>
+                </div>
+                <div
+                    className={`RBDoctorHome__queueTab-item
+                    ${selected === 2 ? 'active' : ''}
+                `}
+                >
+                    Qua lượt <span>(20)</span>
+                </div>
+            </div>
+            <div className="RBDoctorHome__queueList">
+                <QueueItem
+                    avatar={createAvatar('Truong Bao')}
+                    pname="Truong Yen Lan"
+                    cid={1234}
+                    pnum={12}
+                />
+            </div>
+        </RightBar>
+    );
 }
 
 RightBarContent.propTypes = {};
 
-export default RightBarContent;
+export default memo(RightBarContent);
